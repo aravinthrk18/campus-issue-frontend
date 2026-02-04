afe506dd0fbfe575b1e6e697c3b489647df5096d
aravinthrk18 <aravinthkannan6669@gmail.com>
2026-02-04 22:09:23 +0530

    Fix StudentDashboard casing issue and clean temp file

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./StudentDashboard.css";
+import "./StudentDashBoard.css";
 
 function StudentDashboard() {
   const [title, setTitle] = useState("");
